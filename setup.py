e99c4aba8480cff8616c33268f2c293bc30a67e6
Sanyn <31801249+Sanyn@users.noreply.github.com>
2020-02-14 20:32:02 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AIM-Sanyn", # Replace with your own username
+    version="0.0.1",
+    author="Zsombor Papp",
+    author_email="pzsombor2002@gmail.com",
+    description="AIM is a python library, that can handle (almost) infinitely big or small numbers. It was devloped for the Papp algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sanyn/AIM",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
